e33034af9d7acf155d7e235e65a39106b95baec3
heather <hjohnsick@yahoo.com>
2020-02-24 21:01:19 -0500

    CoffeeShopLab

---
+++ CoffeeShopLab/Controllers/RegistrationController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoffeeShopLab.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CoffeeShopLab.Controllers
+{
+    public class RegistrationController : Controller
+    {
+        
+        public IActionResult RegistrationForm()
+        {
+            return View(new RegisterUser());
+        }
+
+        public IActionResult RegistrationResult(RegisterUser registerUser)
+        {
+            if (ModelState.IsValid)
+            {
+                return View(registerUser); /*returning registerUser*/
+            }
+            else
+                return View("RegistrationForm", registerUser);
+        }
+
+       
+    }
+}
---
+++ CoffeeShopLab/Models/RegisterUser.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoffeeShopLab.Models
+{
+    public class RegisterUser
+    {
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 30 characters.")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "Name must be between 2 and 30 characters.")]
+        public string LastName { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9{5,30}]+@[a-zA-A0-9{5,10}]+\.[a-zA-Z0-9{2,3}]+$", ErrorMessage ="Please enter a valid email.")]
+        public string Email { get; set; }
+        [Required]
+        public string Password { get; set; }
+        [Required]
+        public string CancelMembership { get; set; }
+
+        public RegisterUser()
+        {
+
+        }
+        public RegisterUser(string firstName, string lastName, string userName, string email, string password, string cancelMembership)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            UserName = userName;
+            Email = email;
+            Password = password;
+            CancelMembership = cancelMembership;
+        }
+
+
+    }
+}
